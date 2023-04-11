e4d9cbb1ef8c1b7964a9220f569c2dc18e3824c5
Sahil pillania <sahilpillania88@gmail.com>
2023-04-11 07:40:39 +0000

    component created

---
+++ src/components/musicPlayer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./music.css"
+const musicPlayer = () => {
+  return (
+    <>
+        <div className="main">
+            <div className="musicplayer">
+                <div className="display"></div>
+                <div className="buttonBox"></div>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default musicPlayer
